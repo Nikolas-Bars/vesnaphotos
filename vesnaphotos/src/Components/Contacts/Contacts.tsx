55acffe9cc6aad1c168934d5c6bbb2d3fdc075fc
Nikolas-Bars <kasp2409@mail.ru>
2022-07-07 19:28:41 +0300

    добавлена анимация с акфьук motion

--- vesnaphotos/src/Components/Contacts/Contacts.tsx
+++ vesnaphotos/src/Components/Contacts/Contacts.tsx
@@ -1,15 +1,33 @@
 import React from 'react';
 import s from './Contacts.module.css'
-import instaLogo from '../../img/instagram.png'
-import vkLogo from '../../img/vk.png'
+import {motion} from 'framer-motion'
+
+const textAnimation = {
+    hidden: {
+        y: 100,
+        opacity: 0,
+    },
+    visible: (custom: number) =>({
+        y: 0,
+        opacity: 1,
+        transition: {delay: custom * 0.2} // функция для настройки времени анимации
+    })
+}
 
 const Contacts = () => {
     return (
-        <div className={s.main}>
-            <div className={s.text}>Наталья Весна</div>
+        <motion.div
+            custom={1}
+            variants={textAnimation}
+            initial='hidden'
+            whileInView='visible'
+            className={s.main}>
+
+            <div   className={s.text}>Наталья Весна</div>
             <div className={s.text}>свадебный и портретный фотограф</div>
-            <div className={s.text}>+7 (999)-827-29-34</div>
-        </div>
+            <div className={s.phoneNumber}>+7 (999)-827-29-34</div>
+
+        </motion.div>
     );
 };
 
--- vesnaphotos/src/Components/Footer/Footer.tsx
+++ vesnaphotos/src/Components/Footer/Footer.tsx
@@ -2,14 +2,32 @@ import React from 'react';
 import instaLogo from "../../img/instagram.png";
 import vkLogo from "../../img/vk.png";
 import s from './Footer.module.css'
+import {motion} from 'framer-motion'
+
+const textAnimation = {
+    hidden: {
+        y: 100,
+        opacity: 0,
+    },
+    visible: (custom: number) =>({
+        y: 0,
+        opacity: 1,
+        transition: {delay: custom * 0.2} // функция для настройки времени анимации
+    })
+}
 
 export const Footer = () => {
     return (
-        <p className={s.main}>
-            <div className={s.imageBlock}><img width={50} src={instaLogo}/> <img width={50} src={vkLogo}/> </div>
+        <motion.p
+            custom={1}
+            variants={textAnimation}
+            initial='hidden'
+            whileInView='visible'
+            className={s.main}>
+            <div className={s.imageBlock}><img width={25} src={instaLogo}/> <img width={25} src={vkLogo}/> </div>
             <span className={s.imageBlock}>Профессиональный фотограф Наталья Весна | Москва | +7 (999) 827 29 34 |   </span><br/>
             <span className={s.imageBlock}>Свадебный фотограф | Детский фотограф | Семейный фотограф |  Фотограф на крещение |   </span>
             <div >Фотосессия беременности  </div>
-        </p>
+        </motion.p>
     );
 };
